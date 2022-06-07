d11c6e16fc686cc415ca007df21ad63d734c17fa
Davide <franc.davi00@gmail.com>
2022-06-07 17:38:34 +0200

    sistemata dir

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <newComponent/>
+    
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+import newComponent from './components/newComponent.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    newComponent
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/newComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>La mia prima app con vue CLI</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newComponent',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  color: red;  
+}
+
+</style>
